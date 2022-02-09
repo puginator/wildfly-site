9006975dc9644dbe1da3f7bf56a204f4184477d8
Austin Amento <puggsy31@aol.com>
2022-02-08 16:50:31 -0800

    added social icons to footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,9 +2,15 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <footer>
-            <div className="container-fluid bg-secondary text-white p-5">
+        <footer className="container-fluid bg-secondary text-center text-white p-5">
+            <div >
                 <h5 className="mb-0">Want to be the first to know about all things Wild Fly?</h5>
+                <input type="text" className="form-control w-50" placeholder="Enter your email address to join our list" />
+            </div>
+            <div>
+                <i class="fa-brands fa-instagram p-3"></i>
+                <i class="fa-brands fa-twitter p-3"></i>
+                <i class="fa-regular fa-envelope p-3"></i>
             </div>
         </footer>
     )
