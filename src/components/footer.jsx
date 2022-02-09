b5068cca18ef9b460b2139321d395c0cfad32289
Austin Amento <puggsy31@aol.com>
2022-02-08 17:04:44 -0800

    cleaned up footer styling

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <footer className="container-fluid bg-secondary text-center text-white p-5">
-            <div >
+        <footer className="container-fluid bg-secondary text-center text-white p-5 justify-content-md-center">
+            <form className="">
                 <h5 className="mb-0">Want to be the first to know about all things Wild Fly?</h5>
-                <input type="text" className="form-control w-50" placeholder="Enter your email address to join our list" />
-            </div>
+                <h5 className="mb-3">Join our email list below.</h5>
+                <input type="email" className="form-control w-25 m-auto rounded-0" placeholder="Enter your email address" />
+                <button className="btn btn-dark w-25 rounded-0">Submit</button>
+            </form>
             <div>
-                <i class="fa-brands fa-instagram p-3"></i>
-                <i class="fa-brands fa-twitter p-3"></i>
-                <i class="fa-regular fa-envelope p-3"></i>
+                <i className="fa-brands fa-instagram p-3 fa-2x text-dark"></i>
+                <i className="fa-brands fa-twitter p-3 fa-2x text-dark"></i>
+                <i className="fa-regular fa-envelope p-3 fa-2x text-dark"></i>
             </div>
         </footer>
     )
