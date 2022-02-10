554b70a205a3f5249f71b5e8ac730f4d9123f333
Austin Amento <puggsy31@aol.com>
2022-02-10 09:52:33 -0800

    added hover to social icons

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -7,7 +7,7 @@ export default function About() {
             <p>Wild Fly Brewing is a nano-brewery in the Kine Kombucha building - brewing very, very small batches.</p>
             <p>We love beer, and as a licensed microbrewery for Kine, we decided to have some fun brewing tiny experimental batches of beer under the Wild Fly brand to share with some of our long time beer friends that enjoy having fun with beer as much as we do.</p>
             <p>Follow us on instagram for beer release info.</p>
-            <a href="https://www.instagram.com/WildFlyBrewing/" target="_blank" rel="noopener noreferrer" className="btn btn-primary">Follow @wildflybrewing<i className="fa-brands fa-instagram p-2"></i> </a>
+            <a href="https://www.instagram.com/WildFlyBrewing/" target="_blank" rel="noopener noreferrer" className="btn btn-primary">Follow @wildflybrewing <i className="fa-brands fa-instagram p-1"></i> </a>
         </div>
     )
 };
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -10,14 +10,14 @@ export default function Footer() {
                 <MailchimpFormContainer />
             </div>
             <div className="m-4">
-                <a href="https://www.instagram.com/WildFlyBrewing/" target="_blank" rel="noopener noreferrer">
-                    <i className="fa-brands fa-instagram p-3 fa-2x text-dark"></i>
+                <a href="https://www.instagram.com/WildFlyBrewing/" target="_blank" rel="noopener noreferrer" >
+                    <i className="fa-brands fa-instagram p-3 fa-2x text-dark social"></i>
                 </a>
                 <a href="https://www.twitter.com/WildFlyBrewing/" target="_blank" rel="noopener noreferrer">
-                    <i className="fa-brands fa-twitter p-3 fa-2x text-dark"></i>
+                    <i className="fa-brands fa-twitter p-3 fa-2x text-dark social"></i>
                 </a>
                 <a href="mailto: wildflybrewing@gmail.com" target="_blank" rel="noopener noreferrer">
-                    <i className="fa-regular fa-envelope p-3 fa-2x text-dark"></i>
+                    <i className="fa-regular fa-envelope p-3 fa-2x text-dark social"></i>
                 </a>
             </div>
         </footer>
